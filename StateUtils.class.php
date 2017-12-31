7bf737cb7710b8b2d9cb64641b9b9d55b8ad0d50
Enda Xue <xueenda@gmail.com>
2017-12-31 12:00:52 -0500

    Prevent some input errors

--- StateUtils.class.php
+++ StateUtils.class.php
@@ -2,6 +2,9 @@
 
 date_default_timezone_set('America/New_York');
 
+if (class_exists('SomeObject'))
+  require_once __DIR__ . '/SomeObject.class.php';
+
 class StateUtils {
 
   /*
@@ -10,9 +13,15 @@ class StateUtils {
    * @return int
    */
   public static function calculateTimeInState($testObject, $state = 'RUNNING') {
+    if(!is_a($testObject, 'SomeObject'))
+      throw new InvalidArgumentException('$testObject was not of type SomeObject');
+
     $statusLog = $testObject->getStatusLog();
     $startDate = $testObject->getStartDate();
     $stopDate = $testObject->getStopDate();
+
+    if(!$statusLog || !$statusLog[0])
+      return 0;
 
     // Index $s: the start position in statsLog
     // Index $e: the end position in statsLog
@@ -44,7 +53,7 @@ class StateUtils {
     if($open > $close || ($open >=0 && $close ==-1))
       $duration += ($stopDate ? $stopDate : time()) - $statusLog[$open]['date'];
 
-    // Edge case: Start date is greater than any log and the last state is open
+    // Edge case: Start date is greater than any logs and the last state is open
     if($startDate && $open == -1 && $close == -1 && 
         $statusLog[count($statusLog) - 1]['newState'] == $state)
       $duration = time() - $startDate;
